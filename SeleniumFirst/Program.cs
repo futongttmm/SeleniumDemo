fff6d58f93e84fdf3a3195a2622de827080f4801
futongttmm <futong101@gmail.com>
2019-06-19 22:44:35 +0800

    First commit

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            //driver.Navigate().GoToUrl("http://www.executeautomation.com/demosite/index.html?UserName=&Password=&Login=Login");
+            driver.Navigate().GoToUrl("http://www.google.com");
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //how to find the right element???
+            SeleniumFirst.SeleniumSetMethod.EnterText(driver, "fakebox-input", "ABCDEFG", "Id");
+
+            /*
+            //Title
+            SeleniumFirst.SeleniumSetMethod.SelectDropDown(driver, "TitleId", "Mr.", "Id");
+            //Initial
+            SeleniumFirst.SeleniumSetMethod.EnterText(driver, "Initial", "Futong", "Name");
+            //check whether the input is what we expected
+            Console.WriteLine(SeleniumFirst.SeleniumGetMethod.GetText(driver, "TitleId", "Id"));
+            Console.WriteLine(SeleniumFirst.SeleniumGetMethod.GetText(driver, "Initial", "Name"));
+
+            //Button
+            SeleniumFirst.SeleniumSetMethod.Click(driver, "Save", "Name");
+            */
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SeleniumFirst/SeleniumSetMethod.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class SeleniumSetMethod
+    {
+        //Enter text
+        public static void EnterText(IWebDriver driver, string element, string inputText, string elementType)
+        {
+            if (elementType == "Id")
+                driver.FindElement(By.Id(element)).SendKeys(inputText);
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(inputText);
+        }
+
+        //Click button, checkbox, options, etc
+        public static void Click(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+                driver.FindElement(By.Id(element)).Click();
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+
+        //Selecting a drop down control
+        public static void SelectDropDown(IWebDriver driver, string element, string inputText, string elementType)
+        {
+            if (elementType == "Id")
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(inputText);
+            if (elementType == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(inputText);
+        }
+    }
+}
